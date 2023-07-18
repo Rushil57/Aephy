c6e46c0fb68395b8ca4c1462c3cff4199803aad7
Rushil <rushilbhuptani1088@gmail.com>
2023-07-19 00:52:25 +0530

    Created "Solutions" page in Admin panel

---
+++ Aephy.WEB.Admin/Controllers/SolutionsController.cs
@@ -0,0 +1,28 @@
+﻿using Aephy.WEB.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aephy.WEB.Admin.Controllers
+{
+    public class SolutionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Solution() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<JsonResult> addSolutionForm([FromBody]SolutionsModel solutionModel) {
+            if (solutionModel != null)
+            {
+                return Json(new { message = "you have selected \n Solution Title : " + solutionModel.SolutionTitle + "\n Solution Subtitle : " + solutionModel.Solution_SubTitle + "\n Solution Description : " + solutionModel.SolutionDescription + "\n Solution Image : " + solutionModel.SolutionImage + "\n Services : " + solutionModel.Services + "\n Industries : " + solutionModel.Industries });
+            }
+            else
+            {
+                return Json(new { message = "failed to receive data.." });
+            }
+        }
+    }
+}
---
+++ Aephy.WEB.Admin/Models/SolutionsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Aephy.WEB.Admin.Models
+{
+    public class SolutionsModel
+    {
+        public string? SolutionId { get; set; }
+        public string? SolutionTitle { get; set;}
+        public string? Solution_SubTitle { get; set;}
+        public string? SolutionDescription { get; set; }
+        public string? SolutionImage { get; set; }
+        public string[]? Services { get; set;}
+        public string[]? Industries { get; set;}
+    }
+}

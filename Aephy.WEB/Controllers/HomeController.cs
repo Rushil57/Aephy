a7c9dcd48e9d5a718130e49e4f4f5d5d6c55f0a3
Rushil <rushilbhuptani1088@gmail.com>
2023-07-27 18:53:39 +0530

    changes in "freelancers dashboard"

--- Aephy.WEB/Controllers/HomeController.cs
+++ Aephy.WEB/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using Aephy.WEB.Models;
 using Aephy.WEB.Provider;
 using Aephy.WEB.Repository;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using Azure.Storage;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
@@ -17,18 +20,22 @@ using static System.Net.WebRequestMethods;
 namespace Aephy.WEB.Controllers
 {
     public class HomeController : Controller
-    {
-
+    { 
         private readonly IApiRepository _apiRepository;
+        private const string ContainerName = "cvfiles";
         private readonly string _rootPath;
         private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
 
         public HomeController(IApiRepository apiRepository, IWebHostEnvironment hostEnvironment, IConfiguration configuration)
         {
             _apiRepository = apiRepository;
-            _rootPath = hostEnvironment.WebRootPath;
             _configuration = configuration;
+            _connectionString = configuration.GetConnectionString("AzureBlobStorage");
+            _rootPath = hostEnvironment.WebRootPath;
+            _connectionString = "DefaultEndpointsProtocol=https;AccountName=aephystorageaccount;AccountKey=nEy6xh4P4m2d94iDgqq+yNB99bucjGMD1wp2L6sbsNFjHPaUQiCHgc5b4hmBmeRtYsiA/WvudVmV+AStwz3djw==;EndpointSuffix=core.windows.net";
         }
+
         public IActionResult Dashboard()
         {
             var userId = HttpContext.Session.GetString("LoggedUser");
@@ -201,6 +208,13 @@ namespace Aephy.WEB.Controllers
             return "";
         }
 
+        [HttpGet]
+        public async Task<string> GetRolesList()
+        {
+            var RolesList = await _apiRepository.MakeApiCallAsync("api/Admin/RolesList", HttpMethod.Get);
+            return RolesList;
+        }
+
         public async Task<IActionResult> VerifyAccount(string userId)
         {
             ViewBag.Active = false;
@@ -224,5 +238,195 @@ namespace Aephy.WEB.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<string> ApplyForOpenGigRoles(IFormFile httpPostedFileBase, string GigOpenRolesData)
+        {
+            try
+            {
+
+                IFormFile CVFile = httpPostedFileBase;
+                var result = JsonConvert.DeserializeObject<OpenGigRolesModel>(GigOpenRolesData);
+
+
+                var freelancer = HttpContext.Session.GetString("LoggedUser");
+                result.FreelancerID = freelancer;
+                var currentDateTime = DateTime.Now;
+                result.CreatedDateTime = currentDateTime;
+                var openGigRolesData = await _apiRepository.MakeApiCallAsync("api/Freelancer/OpenGigRolesApply", HttpMethod.Post, result);
+                dynamic data = JsonConvert.DeserializeObject(openGigRolesData);
+                if (data != null)
+                {
+                    if (data.Message == "Applied Successfully")
+                    {
+                        int Id = data.Result;
+                        var d = await SaveCVFile(CVFile, Id);
+                        var ok = await _apiRepository.MakeApiCallAsync("api/Freelancer/UpdateCV", HttpMethod.Post, d);
+                        dynamic ImageResponse = JsonConvert.DeserializeObject(ok);
+                        if (ImageResponse != null)
+                        {
+                            return ImageResponse.Message;
+                        }
+                        else
+                        {
+                            return "Failed to Apply !";
+                        }
+                    }
+                    else
+                    {
+                        return data.Message;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "Failed to Apply !";
+        }
+        private static string GetEndpointSuffixFromConnectionString(string connectionString)
+        {
+            string[] parts = connectionString.Split(";");
+            foreach (var part in parts)
+            {
+                string[] subparts = part.Split("=");
+                if (subparts.Length == 2 && subparts[0].Equals("EndpointSuffix"))
+                {
+                    return subparts[1];
+                }
+            }
+            return null;
+        }
+
+        private string GenerateSasToken(string imageUrl)
+        {
+            // Get the blob container name and blob name from the image URL
+            string blobName = Path.GetFileName(imageUrl);
+
+            // Create a shared access policy that allows read access to the blob
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = ContainerName,
+                BlobName = blobName,
+                Resource = "b",
+                StartsOn = DateTime.UtcNow.AddMinutes(-5), // Adjust the start time if needed
+                ExpiresOn = DateTime.UtcNow.AddMinutes(10), // Adjust the expiry time if needed,
+                Protocol = SasProtocol.Https
+            };
+            sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+            // Get the Azure Blob Storage connection string from configuration
+            var connectionString = _configuration.GetConnectionString("AzureBlobStorage");
+
+            // Extract the AccountName and AccountKey from the connection string
+            var accountName = GetAccountNameFromConnectionString(connectionString);
+            var accountKey = GetAccountKeyFromConnectionString(connectionString);
+
+            // Create a StorageSharedKeyCredential using the AccountName and AccountKey
+            StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
+
+            // Generate the SAS token
+            string sasToken = sasBuilder.ToSasQueryParameters(credential).ToString();
+
+            return sasToken;
+        }
+
+
+        private string GetAccountNameFromConnectionString(string connectionString)
+        {
+            try
+            {
+                var accountNameStartIndex = connectionString.IndexOf("AccountName=", StringComparison.InvariantCultureIgnoreCase) + "AccountName=".Length;
+                var accountNameEndIndex = connectionString.IndexOf(";", accountNameStartIndex, StringComparison.InvariantCultureIgnoreCase);
+                var accountNameLength = accountNameEndIndex - accountNameStartIndex;
+                return connectionString.Substring(accountNameStartIndex, accountNameLength);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            return "";
+        }
+
+        private string GetAccountKeyFromConnectionString(string connectionString)
+        {
+            try
+            {
+                var accountKeyStartIndex = connectionString.IndexOf("AccountKey=", StringComparison.InvariantCultureIgnoreCase) + "AccountKey=".Length;
+                var accountKeyEndIndex = connectionString.IndexOf(";", accountKeyStartIndex, StringComparison.InvariantCultureIgnoreCase);
+                var accountKeyLength = accountKeyEndIndex - accountKeyStartIndex;
+                return connectionString.Substring(accountKeyStartIndex, accountKeyLength);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            return "";
+        }
+
+        public async Task<OpenGigRolesCV> SaveCVFile(IFormFile CVFile, object Id)
+        {
+            OpenGigRolesCV opengigroles = new OpenGigRolesCV();
+            try
+            {
+                if (CVFile != null && CVFile.Length > 0)
+                {
+                    string BlobStorageBaseUrl = string.Empty;
+                    string CVPath = string.Empty;
+                    string CVUrlWithSas = string.Empty;
+
+                    string fileName = Guid.NewGuid().ToString() + "_" + CVFile.FileName;
+
+                    // Get the Azure Blob Storage connection string from configuration
+                    var connectionString = _configuration.GetConnectionString("AzureBlobStorage");
+
+                    BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+                    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(ContainerName);
+
+                    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+                    using (var stream = CVFile.OpenReadStream())
+                    {
+                        await blobClient.UploadAsync(stream, overwrite: true);
+                    }
+
+                    CVPath = blobClient.Uri.ToString();
+
+
+                    string sasToken = GenerateSasToken(CVPath);
+
+
+                    string cvUrlWithSas = CVPath + sasToken;
+
+
+
+                    BlobStorageBaseUrl = containerClient.Uri.ToString();
+
+
+                    CVUrlWithSas = cvUrlWithSas;
+
+                    opengigroles.BlobStorageBaseUrl = BlobStorageBaseUrl;
+                    opengigroles.CVPath = CVPath;
+                    opengigroles.CVUrlWithSas = CVUrlWithSas;
+                    opengigroles.ID = (int)(Id);
+
+                    return opengigroles;
+                }
+                else
+                {
+                    ModelState.AddModelError("ImageFile", "Please select an image file.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return opengigroles;
+
+
+        }
     }
 }
